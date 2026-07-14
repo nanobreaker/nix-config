8bab185a87467e2c30c777178059e7d66be5f6fb
nanobreaker <nan0br3aker@gmail.com>
2026-07-14 10:33:05 +0300

    flake: add nixos-raspberrypi cachix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
-  description = "thatwhichisnot's nixos flake based configuration";
+  description = "thatwhichisdev's personalized NixOS configuration";
 
   nixConfig = {
     extra-substituters = [
       "https://nixos-apple-silicon.cachix.org"
       "https://nix-community.cachix.org/"
       "https://noctalia.cachix.org"
+      "https://nixos-raspberrypi.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
     ];
 
     experimental-features = [

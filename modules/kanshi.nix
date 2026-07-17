c315c69723a88fbbfb633b3bbd27d96f0e546ab4
nanobreaker <nan0br3aker@gmail.com>
2026-07-17 14:51:36 +0300

    kanshi: update

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -1,4 +1,5 @@
 { inputs, pkgs, ... }:
+
 let
   awww = inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww;
 in
@@ -10,46 +11,50 @@ in
         package = pkgs.kanshi;
         systemdTarget = "graphical-session.target";
 
-        profiles = {
-          laptop = {
-            name = "laptop";
+        settings = [
+          {
+            profile = {
+              name = "laptop";
 
-            outputs = [
-              {
-                criteria = "eDP-1";
-                status = "enable";
-                mode = "3456x2160@120.000";
-                position = "2560,0";
-                scale = 2.0;
-              }
-            ];
-          };
+              outputs = [
+                {
+                  criteria = "eDP-1";
+                  status = "enable";
+                  mode = "3456x2160@120.000";
+                  position = "2560,0";
+                  scale = 2.0;
+                }
+              ];
+            };
+          }
 
-          docked = {
-            name = "docked";
+          {
+            profile = {
+              name = "docked";
 
-            outputs = [
-              {
-                criteria = "DP-1";
-                status = "enable";
-                mode = "2560x1440@239.958";
-                position = "0,0";
-                scale = 1.0;
-              }
-              {
-                criteria = "eDP-1";
-                status = "enable";
-                mode = "3456x2160@120.000";
-                position = "2560,0";
-                scale = 2.0;
-              }
-            ];
+              outputs = [
+                {
+                  criteria = "DP-1";
+                  status = "enable";
+                  mode = "2560x1440@239.958";
+                  position = "0,0";
+                  scale = 1.0;
+                }
+                {
+                  criteria = "eDP-1";
+                  status = "enable";
+                  mode = "3456x2160@120.000";
+                  position = "2560,0";
+                  scale = 2.0;
+                }
+              ];
 
-            exec = [
-              "${awww}/bin/awww img ${inputs.nix-assets.assets.wallpapers.motion.waneella-clouds}"
-            ];
-          };
-        };
+              exec = [
+                "${awww}/bin/awww img ${inputs.nix-assets.assets.wallpapers.motion.waneella-clouds}"
+              ];
+            };
+          }
+        ];
       };
     }
   ];

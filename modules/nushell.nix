1e565f755ceab7081096073123e3a5106969952a
nanobreaker <nan0br3aker@gmail.com>
2026-07-22 11:12:12 +0300

    nushell: remove table decoration

--- modules/nushell.nix
+++ modules/nushell.nix
@@ -15,7 +15,7 @@
         configFile.text = ''
           $env.config.show_banner = false
           $env.config.buffer_editor = 'hx'
-          $env.config.table.mode = 'ascii_rounded'
+          $env.config.table.mode = 'none'
 
           alias ll = ls -la
           alias fg = job unfreeze
